5ccd81b36b1b9b824d2637b500a26706675c18e9
prudita <99392091+prudita@users.noreply.github.com>
2023-12-12 17:45:48 +0700

    Update views.py

--- cr_pengguna/views.py
+++ cr_pengguna/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        role = request.POST.get('role')
+
+        if role == 'customer':
+            email = request.POST.get('customerEmail')
+            
+
+        elif role == 'hotel':
+            email = request.POST.get('hotelEmail')
+            
+
+        return HttpResponse(f'Registration successful for {role}')
+
+    return render(request, 'registration_form.html')
+
+
+

ce000a74156617a1e245f05aeb6ceaebae3dc41d
prudita <99392091+prudita@users.noreply.github.com>
2023-12-12 17:42:26 +0700

    Update models.py

--- cr_pengguna/models.py
+++ cr_pengguna/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Customer(models.Model):
+    email = models.EmailField(unique=True)
+
+class Hotel(models.Model):
+    email = models.EmailField(unique=True)
+
 # Create your models here.
